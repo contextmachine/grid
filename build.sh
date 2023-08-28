abf908f618b72868f720223e62fdc726779b427a
sth-v <aa@contextmachine.online>
2023-08-28 03:45:59 +0300

    upd

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker buildx build --no-cache --platform linux/amd64  --tag sthv/mfb_sw_l2:test  --push .
-docker run --platform linux/amd64 --rm --tty -p 7711:7711 sthv/mfb_sw_l2:test
+docker buildx build --no-cache --platform linux/amd64  --tag  cr.yandex/crpfskvn79g5ht8njq0k/mfb_sw_l2:test  --push .
+docker run --platform linux/amd64 --rm --tty -p 7711:7711  cr.yandex/crpfskvn79g5ht8njq0k/mfb_sw_l2:test
 
