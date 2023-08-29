6c033fb52c1f9cb4b36a2018ab2c0dbe31fd3c54
sth-v <aa@contextmachine.online>
2023-08-29 08:06:23 +0300

    fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+chmod +x upload_static.sh
+./upload_static.sh
 docker buildx build --platform linux/amd64  --tag  sthv/mfb_sw_l2:test  --push .
 #docker build --no-cache -t  cr.yandex/crpfskvn79g5ht8njq0k/mfb_sw_l2:test .
 #docker run --platform linux/amd64 --rm --tty -p 7711:7711  cr.yandex/crpfskvn79g5ht8njq0k/mfb_sw_l2:test
